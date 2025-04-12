6eea43d7204e14c7331700a4c546328f371ff64b
Christian Böndgen <c.boendgen@gmail.com>
2025-04-12 16:30:20 +0200

    add init custom gemaps

---
+++ src/age_by_voice/audio/custom_gemaps.py
@@ -0,0 +1,33 @@
+import numpy as np
+import librosa
+import scipy.signal as sp
+
+
+class Custom_GeMAPS:
+    """
+    Custom GeMAPS feature extraction class.
+    This class shows how the features of the GeMAPS feature extraction are calculated.
+
+    Attributes:
+        _audio_path (str): Path to the audio file to analyze.
+    """
+
+    def __init__(
+        self, audio_path: str, sample_rate: int = None, mono: bool = True
+    ) -> None:
+        """
+        Initialize the Custom_GeMAPS class.
+        Args:
+            audio_path (str): Path to the audio file to analyze.
+        """
+        self._audio_path: str = audio_path
+        self._mono: bool = mono
+        self.sr: int = sample_rate
+        self.y = None
+
+        if not self._mono:
+            raise NotImplementedError(
+                "Custom GeMAPS feature extraction only supports mono audio files."
+            )
+
+        self.y, self.sr = librosa.load(self._audio_path, sr=self.sr, mono=self._mono)
