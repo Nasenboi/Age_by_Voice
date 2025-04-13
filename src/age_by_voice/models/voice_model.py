74e52389800441b87682940d42a1b0133ffa3db4
Christian Böndgen <c.boendgen@gmail.com>
2025-04-13 14:25:06 +0200

    add data models

---
+++ src/age_by_voice/models/voice_model.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import Optional, Union, Literal
+
+
+class VoiceModel(BaseModel):
+    clip_id: Union[str, int]
+    voice_name: str
+    voice_age: Optional[int]
+    voice_age_group: Literal[
+        "10", "20", "30", "40", "50", "60", "70", "80", "90", "100"
+    ]
+    voice_gender: Optional[Literal["m", "f"]]
