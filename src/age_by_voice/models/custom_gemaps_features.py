9613ddc44b9d270db87f459d70307399dca19049
Christian Böndgen <c.boendgen@gmail.com>
2025-04-17 15:56:20 +0200

    formatting

--- src/age_by_voice/models/custom_gemaps_features.py
+++ src/age_by_voice/models/custom_gemaps_features.py
@@ -14,10 +14,12 @@ class Loudness(BaseModel):
     loudness_80: float
     loudness_90: float
 
+
 # Zero Crossing Rate
 class Zero_Crossing_Rate(BaseModel):
     zcr_mean: float
     zcr_std: float
+
 
 # Peaks per Second
 class Peaks_Per_Second(BaseModel):
@@ -27,15 +29,18 @@ class Peaks_Per_Second(BaseModel):
     pps_80: float
     pps_90: float
 
+
 # Voiced Segments per second => retrieved from f0 though!
 
 # Unvoiced Segments per second => retrieved from f0 though!
+
 
 # -- Frequency Domain Features --
 # HNR
 class Harmonics_To_Noise_Ratio(BaseModel):
     hnr_mean: float
     hnr_std: float
+
 
 # F0
 class F0(BaseModel):
@@ -46,6 +51,7 @@ class F0(BaseModel):
     f0_90: float
     f0_min: float
     f0_max: float
+
 
 # Additional Spectral Features
 class Additional_Spectral_Features(BaseModel):
@@ -61,6 +67,7 @@ class Additional_Spectral_Features(BaseModel):
     spectral_rolloff_mean: float
     spectral_rolloff_std: float
 
+
 # -- Quefrency Domain Features --
 # MFCC_1-4
 class MFCC_1_4(BaseModel):
@@ -73,8 +80,17 @@ class MFCC_1_4(BaseModel):
     mfcc_4_mean: float
     mfcc_4_std: float
 
-class Custom_GeMAPS_Features(FeaturesModel, Loudness, Zero_Crossing_Rate, Peaks_Per_Second,
-                             Harmonics_To_Noise_Ratio, F0, Additional_Spectral_Features, MFCC_1_4):
+
+class Custom_GeMAPS_Features(
+    FeaturesModel,
+    Loudness,
+    Zero_Crossing_Rate,
+    Peaks_Per_Second,
+    Harmonics_To_Noise_Ratio,
+    F0,
+    Additional_Spectral_Features,
+    MFCC_1_4,
+):
     """
     Custom GeMAPS features model.
     This model is based off the GeMAPS features model implemented in opensmile.
